fbb4b7d8002749eecd339c8eeb14da1b7f0b978b
alix <a1ix2@proton.me>
2026-04-16 16:10:38 -0400

    publish doctor/patient power dynamics article, minor css tweak

--- js/toc.js
+++ js/toc.js
@@ -4,7 +4,6 @@ for (let link of toc.getElementsByTagName("a")) {
     const backlink = document.createElement("a");
     backlink.className = 'citation-backlinks';
     backlink.innerHTML = '↩︎';
-    backlink.style.fontSize = '0.6em';
     backlink.href = "#" + link.id;
 
     document.getElementById(link.getAttribute("href").substr(1)).append(" ", backlink);
