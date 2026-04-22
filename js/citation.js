d8cc6723bf71ce2f18b4bb19c25eb69882e2a83f
alix <a1ix2@proton.me>
2026-04-21 22:24:20 -0400

    formatting, noreferrer noopener

--- js/citation.js
+++ js/citation.js
@@ -4,14 +4,14 @@
 //
 // Copyright (c) 2024 Max Bo
 function alphabetize(n) {
-  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-  let out = "";
-  while (n > 0) {
-    const mod = (n - 1) % 26;
-    out = alphabet[mod] + out;
-    n = Math.floor((n - mod) / 26);
-  }
-  return out;
+    const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+    let out = "";
+    while (n > 0) {
+        const mod = (n - 1) % 26;
+        out = alphabet[mod] + out;
+        n = Math.floor((n - mod) / 26);
+    }
+    return out;
 }
 // end import
 
@@ -30,37 +30,37 @@ for (let entry of document.getElementsByClassName("csl-entry")) {
 
     let i = 1;
     for (let cit of citations) {
-	let backlink = alphabetize(i);
-	// enable backlink
-	cit.setAttribute("id", ref + '-' + backlink);
+        let backlink = alphabetize(i);
+        // enable backlink
+        cit.setAttribute("id", ref + '-' + backlink);
 
-	// backlink
-	const elem = document.createElement('a');
-	elem.textContent = backlink;
-	elem.href = '#' + ref + '-' + backlink;
-	backlinks.append(elem, " ");
+        // backlink
+        const elem = document.createElement('a');
+        elem.textContent = backlink;
+        elem.href = '#' + ref + '-' + backlink;
+        backlinks.append(elem, " ");
 
-	// enable hover action
-	const wrapper = document.createElement('span');
-	// wrap the cit to allow mouseover of both elements combined
-	cit.parentNode.replaceChild(wrapper, cit);
-	wrapper.appendChild(cit);
+        // enable hover action
+        const wrapper = document.createElement('span');
+        // wrap the cit to allow mouseover of both elements combined
+        cit.parentNode.replaceChild(wrapper, cit);
+        wrapper.appendChild(cit);
 
         const tooltip = document.createElement('span');
         tooltip.innerHTML = entry.innerHTML;
-	tooltip.className = 'citation-tooltip';
-	wrapper.appendChild(tooltip);
+        tooltip.className = 'citation-tooltip';
+        wrapper.appendChild(tooltip);
 
         wrapper.addEventListener('mouseenter', () => {
-	    // Position the tooltip underneath the citation link, but further left if necessary
+            // Position the tooltip underneath the citation link, but further left if necessary
             const contentPadding = parseFloat(window.getComputedStyle(document.getElementsByClassName("content-area")[0]).paddingRight, 10);
-	    const contentWidth = window.innerWidth - contentPadding*2;
-	    const boxStyle = window.getComputedStyle(tooltip);
-	    const boxWidth = Math.min(parseFloat(boxStyle.width, 10) + parseFloat(boxStyle.paddingRight)*2 + 2, contentWidth + 2);
-	    const rectStart = cit.getBoundingClientRect().left;
+            const contentWidth = window.innerWidth - contentPadding * 2;
+            const boxStyle = window.getComputedStyle(tooltip);
+            const boxWidth = Math.min(parseFloat(boxStyle.width, 10) + parseFloat(boxStyle.paddingRight) * 2 + 2, contentWidth + 2);
+            const rectStart = cit.getBoundingClientRect().left;
 
-	    const position = (rectStart + boxWidth > contentWidth) ? contentWidth - boxWidth + contentPadding : rectStart;
-	    tooltip.style.left = `${position}px`;
+            const position = (rectStart + boxWidth > contentWidth) ? contentWidth - boxWidth + contentPadding : rectStart;
+            tooltip.style.left = `${position}px`;
             tooltip.style.display = 'block';
         });
 
@@ -68,7 +68,7 @@ for (let entry of document.getElementsByClassName("csl-entry")) {
             tooltip.style.display = 'none';
         });
 
-	i++;
+        i++;
     }
     entry.prepend(backlinks, " ");
 }
